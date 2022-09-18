74ae21c9c379e4fadf7f614efe2c3d3b7a6ec0d1
Varun Wahi <wahivarun02@gmail.com>
2022-09-18 11:49:21 +0530

    added nested switch statement

--- src/com/varun/switchCase.java
+++ src/com/varun/switchCase.java
@@ -3,8 +3,8 @@ package com.varun;
 import java.util.Scanner;
 
 public class switchCase {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         System.out.print("Enter the name of a fruit: ");
         String fruit = in.next();
         //System.out.println(fruit);
@@ -32,7 +32,16 @@ public class switchCase {
         switch (fruit) {
             case "Apple" -> System.out.println("Red Fruit");
             case "Mango" -> System.out.println("Yellow Sweet fruit!");
-            case "Grapes" -> System.out.println("It's a small fruit");
+            case "Grapes" -> {
+                System.out.print("Which Type of Grape? \n1.Black\n2.Green\n: ");
+                int typeOfGrape = in.nextInt();
+                switch (typeOfGrape){
+                    case 1 -> System.out.println("You chose Black Grapes! That's a great choice!");
+                    case 2 -> System.out.println("You chose Green Grapes! That's also a great choice!");
+                    default -> System.out.println("Choose an option between 1 and 2 :/");
+                }
+                System.out.println("It's a small fruit");
+            }
             default -> System.out.println("Enter a valid fruit :/");
         }
     }
