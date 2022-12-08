bf7cdb170c91d5986cb51db1b3050fda57bff9a9
Varun Wahi <wahivarun02@gmail.com>
2022-12-08 21:35:05 +0530

    Making a Pyramid Pattern

--- src/com/varun/Pyramid.java
+++ src/com/varun/Pyramid.java
@@ -7,8 +7,9 @@ public class Pyramid {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of levels: ");
         int lev = scanner.nextInt();
-        for(int i =0; i < lev; i++){
+        for(int i =0; i < lev; i++){ //Printing the pattern
             System.out.println(" ".repeat(lev-i) + "*".repeat(i+1) + "*".repeat(i));
+
         }
     }
 }
